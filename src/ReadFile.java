7692c2378d715939851db69b72ffd3829cafb013
Christian-pprogrammer <mpanoc6@gmail.com>
2022-04-24 13:12:26 +0200

    add read file

--- src/ReadFile.java
+++ src/ReadFile.java
@@ -1,5 +1,14 @@
+import java.io.FileReader;
+
 public class ReadFile {
     public static void main(String[] args) {
-        
+        char [] array = new char[100];
+        try{
+            FileReader input = new FileReader("newFile.txt");
+            input.read(array);
+            System.out.println(array);
+        }catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
